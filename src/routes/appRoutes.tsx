99c6b02f7809047f5d82b40db78b3c12b4287e8b
Huy-techSw <lequanghuy30012003@gmail.com>
2024-09-10 12:53:06 +0700

    first commit

---
+++ src/routes/appRoutes.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const AppRoutes = (props: Props) => {
+  return (
+    <div>AppRoutes</div>
+  )
+}
+
+export default AppRoutes
---
+++ src/services/axiosClient.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosError } from "axios";
+// import { isEmpty } from "lodash";
+
+const axiosClient = axios.create({
+  baseURL: "https://localhost:5001/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+axiosClient.interceptors.request.use(
+  function (config: any) {
+    const token = localStorage.getItem("jwtToken");
+    // Do something before request is sent
+    if (token !== undefined) {
+      // console.log("Token: ", token);
+      if (config.headers) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      } else {
+        config.headers = {
+          Authorization: `Bearer ${token}`,
+        };
+      }
+    }
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+axiosClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error: AxiosError<{ content: string }>) => {
+    return error.response?.data;
+  }
+);
+
+export default axiosClient;
