4c26fb4de422b816e9887a7d181bc2d85064adca
Huy-techSw <120246359>
2024-11-07 16:53:20 +0700

    Updating for all

---
+++ src/pages/FormPage/signUpPage.tsx
@@ -0,0 +1,15 @@
+import Footer from '../../components/footer'
+import RegisterForm from '../../components/form/registerForm'
+import Header from '../../components/header'
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <Header />
+      <RegisterForm />
+      <Footer />
+    </div>
+  )
+}
+
+export default SignUpPage
